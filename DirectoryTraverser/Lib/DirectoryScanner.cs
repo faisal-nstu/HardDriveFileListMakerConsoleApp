26183e87f3bcbb21d7aff22210c2b4f598f90b3d
unknown <faisal-amin>
2015-01-13 14:30:07 +0600

    initial commit

---
+++ DirectoryTraverser/Lib/DirectoryScanner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+
+namespace DirectoryTraverser.Lib
+{
+    public class DirectoryScanner
+    {
+        public static IEnumerable<string> Traverse(string rootDirectory)
+        {
+            IEnumerable<string> files = Enumerable.Empty<string>();
+            IEnumerable<string> directories = Enumerable.Empty<string>();
+            try
+            {
+                // The test for UnauthorizedAccessException.
+                var permission = new FileIOPermission(FileIOPermissionAccess.PathDiscovery, rootDirectory);
+                permission.Demand();
+
+                files = Directory.GetFiles(rootDirectory);
+                directories = Directory.GetDirectories(rootDirectory);
+            }
+            catch
+            {
+                // Ignore folder (access denied).
+                rootDirectory = null;
+            }
+
+            if (rootDirectory != null)
+                yield return rootDirectory;
+
+            foreach (var file in files)
+            {
+                yield return file;
+            }
+
+            // Recursive call for SelectMany.
+            var subdirectoryItems = directories.SelectMany(Traverse);
+            foreach (var result in subdirectoryItems)
+            {
+                yield return result;
+            }
+        }
+    }
+}
---
+++ HardDriveFileListMakerConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectoryTraverser.Lib;
+
+namespace HardDriveFileListMakerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.IsReady)
+                {
+                    var paths = DirectoryScanner.Traverse(d.Name);
+                    File.AppendAllLines(@"D:\SearchResult.txt", paths);
+                }
+            }
+        }
+    }
+}
